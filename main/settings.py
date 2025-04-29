414a820f46a06ab6873b00141d66c695d17b5bf9
Nezdeshniy <nezdeshniy@protonmail.com>
2025-04-29 14:08:03 +0600

    Add Celery

--- main/settings.py
+++ main/settings.py
@@ -129,3 +129,6 @@ MEDIA_ROOT = 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
